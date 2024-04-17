fd341e116c8c2fb468fd4824bbe1ba89bbd8bc63
bokele wakiza franck <bwakiza@gmail.com>
2024-04-17 09:37:25 +0200

    convert the image and save it

--- image.py
+++ image.py
@@ -2,8 +2,16 @@ from PIL import Image,ImageFilter
 
 
 img  = Image.open('./images/pikachu.jpg')
+
+#blur the image
 filter_img = img.filter(ImageFilter.BLUR)
 filter_img.save('blur.png', 'png')
-filter_img.show()
+
+#convert the image
+filter_img_l = img.convert('L')
+filter_img_l.save('grey.png', 'png')
+
+
+# filter_img.show()
 
 # print(dir(img))
