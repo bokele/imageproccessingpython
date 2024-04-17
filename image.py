654e4398eeddaab1f10ef671a5184c0a16b0d1c7
bokele wakiza franck <bwakiza@gmail.com>
2024-04-17 09:38:37 +0200

    rotate nd resize the image

--- image.py
+++ image.py
@@ -11,6 +11,11 @@ filter_img.save('blur.png', 'png')
 filter_img_l = img.convert('L')
 filter_img_l.save('grey.png', 'png')
 
+#rorate the image and resize the image
+image_rotate = filter_img_l.rotate(90)
+image_resize = image_rotate.resize((300,300))
+image_resize.save('grey.png', 'png')
+
 
 # filter_img.show()
 
