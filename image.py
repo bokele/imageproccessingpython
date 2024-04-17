a4cf5d7d199a91b3368742f147370857c3ae5795
bokele wakiza franck <bwakiza@gmail.com>
2024-04-17 09:35:41 +0200

    blur the image and open it

--- image.py
+++ image.py
@@ -2,7 +2,7 @@ from PIL import Image,ImageFilter
 
 
 img  = Image.open('./images/pikachu.jpg')
+filter_img = img.filter(ImageFilter.BLUR)
+filter_img.show()
 
-
-
-print(dir(img))
+# print(dir(img))
