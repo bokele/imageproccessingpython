a97022468bd64459c1a5488a7ce449817bc8581e
bokele wakiza franck <bwakiza@gmail.com>
2024-04-17 09:36:10 +0200

    save the image

--- image.py
+++ image.py
@@ -3,6 +3,7 @@ from PIL import Image,ImageFilter
 
 img  = Image.open('./images/pikachu.jpg')
 filter_img = img.filter(ImageFilter.BLUR)
+filter_img.save('blur.png', 'png')
 filter_img.show()
 
 # print(dir(img))
