dff1de21035052c9d103b3aceef80b09fb3e6fcb
bokele wakiza franck <bwakiza@gmail.com>
2024-04-17 11:29:46 +0200

    combiner pdfs

---
+++ pdf-playground.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import PyPDF2
+
+inputs = sys.argv[1:] 
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+
+    merger.write('new_combiner_pdf.pdf')
+
+
+pdf_combiner(inputs)
